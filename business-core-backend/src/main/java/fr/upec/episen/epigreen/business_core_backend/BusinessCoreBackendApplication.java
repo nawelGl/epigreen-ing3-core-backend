a65b4cec0dec911fac2b4049952ea5b5f73018dc
nawelGl <ghazal.nawel@gmail.com>
2025-12-05 14:11:03 +0100

    Création du projet Spring

---
+++ business-core-backend/src/main/java/fr/upec/episen/epigreen/business_core_backend/BusinessCoreBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.epigreen.business_core_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessCoreBackendApplication.class, args);
+	}
+
+}
---
+++ business-core-backend/src/test/java/fr/upec/episen/epigreen/business_core_backend/BusinessCoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.epigreen.business_core_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessCoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
